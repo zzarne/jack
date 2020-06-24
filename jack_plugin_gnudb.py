2ef2745c2cd2a429c43aefb581dc18c248fde159
Arne Zellentin <zzarne@users.noreply.github.com>
2020-06-24 16:51:48 +0200

    added jack_plugin_gnudb.py: gnudb support for existing installations

---
+++ jack_plugin_gnudb.py
@@ -0,0 +1,10 @@
+### jack_plugin_gnudb: example gnudb server plugin
+
+plugin_freedb_servers = {
+    'plugin_gnudb': {
+        'host': "gnudb.gnudb.org",
+        'id': prog_name + " " + prog_version,
+        'mail': "gnudb-submit@gnudb.org",
+        'my_mail': "default"
+    },
+}
